139590a9e8391f37b049852bb5cecd5f8b2f21fe
Anastasiya <123961941+Anastasiya-Polikanova@users.noreply.github.com>
2023-04-18 16:24:54 +0300

    Add files via upload

---
+++ precache-manifest.8027f059e12fe3085cb80a21d42a8025.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bceaac0c6fe829126ab95b893ce1171",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7e237e6e9d2a31e6765",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "f8cda03a0698feb3be5b",
+    "url": "/static/js/2.612d2c84.chunk.js"
+  },
+  {
+    "revision": "d7e237e6e9d2a31e6765",
+    "url": "/static/js/main.8bfaeff3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
